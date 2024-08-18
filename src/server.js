b039f75905b4ff2e98bb8f48534d053d585cadbf
Waseef Mohammad Khan <waseef2010@gmail.com>
2024-08-18 07:44:27 +0100

    removed prom-client

--- src/server.js
+++ src/server.js
@@ -7,9 +7,6 @@ import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url"; // Import for ES modules
 
-import promClient from "prom-client";
-import { collectDefaultMetrics } from "prom-client";
-
 import { connect } from "./mongo.js"; // Adjust this path if needed
 import apiRoutes from "./routes/api.js"; // Adjust this path if needed
 
@@ -19,28 +16,6 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
-// Create a new Counter metric
-const requestCounter = new promClient.Counter({
-  name: "http_requests_total",
-  help: "Total number of HTTP requests",
-  labelNames: ["method", "route"],
-});
-
-// Collect default metrics (optional)
-collectDefaultMetrics();
-
-// Middleware to increment the request counter
-app.use((req, res, next) => {
-  requestCounter.inc({ method: req.method, route: req.path });
-  next();
-});
-
-// Endpoint to expose metrics to Prometheus
-app.get("/metrics", (req, res) => {
-  res.set("Content-Type", promClient.register.contentType);
-  res.send(promClient.register.metrics());
-});
 
 connect();
 
